dc1e05f5a97be1ec9f444078e247e02413e51abc
Gautham <mvgauthamkrishna@gmail.com>
2024-03-23 02:32:14 +0530

    second

--- myApp.js
+++ myApp.js
@@ -54,7 +54,7 @@ app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 3030;
 app.listen(port, () => {
   console.log(`Info Security started on port ${port}`);
   });
